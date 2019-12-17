466cd6c54c0db93d4f762e7a674f22ca89c94dd1
AnkitVerma8584 <av8584060@gmail.com>
2019-12-17 21:56:46 +0530

    Added student subjects selection. fixed login issue

--- app/src/main/java/com/example/attendancemanager/MainActivity.java
+++ app/src/main/java/com/example/attendancemanager/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.attendancemanager;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,43 +16,67 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {      //MainActivity is the Login Activity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     FirebaseAuth auth;
     EditText ue,up;
     Button sign;
     String useremail,userpassword;
     ProgressDialog pd;
     TextView tvreg;
-    DatabaseReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sign=findViewById(R.id.button);
+        sign=findViewById(R.id.signin);
         ue=findViewById(R.id.email);
         up=findViewById(R.id.password);
         sign.setOnClickListener(this);
         auth=FirebaseAuth.getInstance();
         pd=new ProgressDialog(this);
-        tvreg=findViewById(R.id.textView2);
+        tvreg=findViewById(R.id.register);
         tvreg.setOnClickListener(this);
-        if(auth.getCurrentUser()!=null){
-            calllogin();
-        }
-        db=FirebaseDatabase.getInstance().getReference().child("Types");
+        /*if(auth.getCurrentUser()!=null){
+            finish();
+            Intent it=new Intent(MainActivity.this,User.class);
+            startActivity(it);
+        }*/
     }
 
     @Override
     public void onClick(View v) {
         if(v==sign)
         {
-            Login();
+            useremail=ue.getText().toString().trim();
+            userpassword=up.getText().toString().trim();
+            if(useremail.equals(""))
+            {
+                Toast.makeText(getApplicationContext(),"Email Id cannot be Blank",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(userpassword.equals(""))
+            {
+                Toast.makeText(getApplicationContext(),"Password cannot be Blank",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            pd.setMessage("Logging in...");
+            pd.show();
+            auth.signInWithEmailAndPassword(useremail,userpassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            pd.dismiss();
+                            if (task.isSuccessful()) {
+                                finish();
+                                Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                                calllogin();//Call User Page
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Login Unsuccessful. Please Check email and password", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
         }
         if(v==tvreg)
         {
@@ -65,41 +85,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
-    private void Login()
-    {
-        String uem=ue.getText().toString();
-        String upa=up.getText().toString();
-        if(uem.equals(""))
-        {
-            Toast.makeText(getApplicationContext(),"Please Provide your Email ID",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if(upa.equals(""))
-        {
-            Toast.makeText(getApplicationContext(),"Please provide a password",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        pd.setMessage("Logging in...");
-        pd.show();
-        auth.signInWithEmailAndPassword(uem,upa)
-                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        pd.dismiss();
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-                            //calllogin();//Call User Page
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Login Unsuccessful. Please Check email and password", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                });
-
-    }
     private void calllogin()
     {
-
+       /*Intent it=new Intent(MainActivity.this,Login.class);
+       startActivity(it);
+       */
     }
 }
-
---
+++ app/src/main/java/com/example/attendancemanager/Studentsubs.java
@@ -0,0 +1,107 @@
+package com.example.attendancemanager;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class Studentsubs extends AppCompatActivity {
+    private Button save;
+    private CheckBox MATHS,PHYS,PHLAB,CHEM,CHLAB,ELEC,ELECLAB,ECE,ECELAB,MECH,MECHLAB,HMTS,CSEN,CSLAB;
+    String subjects[]=new String[14],Subs="";
+    int i=0,a=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studentsubs);
+        save=findViewById(R.id.saveinfo);
+        //Linking all the subjects
+        MATHS=findViewById(R.id.maths);
+        PHYS=findViewById(R.id.physics);
+        PHLAB=findViewById(R.id.phylab);
+        CHEM=findViewById(R.id.chemistry);
+        CHLAB=findViewById(R.id.chemlab);
+        ELEC=findViewById(R.id.electrical);
+        ELECLAB=findViewById(R.id.eleclab);
+        ECE=findViewById(R.id.electronics);
+        ECELAB=findViewById(R.id.ecelab);
+        MECH=findViewById(R.id.mechanical);
+        MECHLAB=findViewById(R.id.mechlab);
+        HMTS=findViewById(R.id.humanities);
+        CSEN=findViewById(R.id.computer);
+        CSLAB=findViewById(R.id.cslab);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog();
+            }
+        });
+
+    }
+    public void Checkon() {
+
+        if (MATHS.isChecked())
+            subjects[i++] = MATHS.getText().toString();
+        if (PHYS.isChecked())
+            subjects[i++] = PHYS.getText().toString();
+        if (PHLAB.isChecked())
+            subjects[i++] = PHLAB.getText().toString();
+        if (CHEM.isChecked())
+            subjects[i++] = CHEM.getText().toString();
+        if (CHLAB.isChecked())
+            subjects[i++] = CHLAB.getText().toString();
+        if (ELEC.isChecked())
+            subjects[i++] = ELEC.getText().toString();
+        if (ELECLAB.isChecked())
+            subjects[i++] = ELECLAB.getText().toString();
+        if (ECE.isChecked())
+            subjects[i++] = ECE.getText().toString();
+        if (ECELAB.isChecked())
+            subjects[i++] = ECELAB.getText().toString();
+        if (CSEN.isChecked())
+            subjects[i++] = CSEN.getText().toString();
+        if (CSLAB.isChecked())
+            subjects[i++] = CSLAB.getText().toString();
+        if (MECH.isChecked())
+            subjects[i++] = MECH.getText().toString();
+        if (MECHLAB.isChecked())
+            subjects[i++] = MECHLAB.getText().toString();
+        if (HMTS.isChecked())
+            subjects[i++] = HMTS.getText().toString();
+
+
+
+    }
+    void Dialog()
+    {    Checkon();
+        Subs="";
+        for(a=0;a<i;a++)
+        { Subs= Subs+subjects[a]+",";
+        }
+        AlertDialog.Builder alt=new AlertDialog.Builder(this);
+        alt.setTitle("Are you sure you want to proceed with the below subjects?")
+                .setCancelable(false)
+                .setMessage(Subs)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                      // savedata(); Saved the subjects to database
+                    }
+                });
+        AlertDialog a=alt.create();
+        a.show();
+    }
+}
+
