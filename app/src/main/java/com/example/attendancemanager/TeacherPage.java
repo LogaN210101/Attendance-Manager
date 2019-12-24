8de7d09a4c714a808c50aa69b19236069265e4a2
AnkitVerma8584 <av8584060@gmail.com>
2019-12-24 18:25:04 +0530

    Updated  teacher page.

--- app/src/main/java/com/example/attendancemanager/TeacherPage.java
+++ app/src/main/java/com/example/attendancemanager/TeacherPage.java
@@ -6,15 +6,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class TeacherPage extends AppCompatActivity {
     Button logout,proceed;
     private RadioGroup r;
     private RadioButton opt;
+    private Spinner dpt;
+    Button nxt;
+    EditText paper, year,sec;
+    String dept;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,31 @@ public class TeacherPage extends AppCompatActivity {
         logout=findViewById(R.id.lg);
         proceed=findViewById(R.id.next);
         r=findViewById(R.id.radiogroup);
+        dpt=findViewById(R.id.department);
+        nxt=findViewById(R.id.next);
+        paper=findViewById(R.id.Papercode);
+        year=findViewById(R.id.batchyear);
+        sec=findViewById(R.id.section);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.Department, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dpt.setAdapter(adapter1);
+        dpt.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                dept = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        nxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TeacherPage.this,TakeAttendance.class));
+            }
+        });
         SharedPreferences.Editor obj =getSharedPreferences("MyData",MODE_PRIVATE).edit();
         obj.putString("Type","Teacher");
         obj.commit();
@@ -52,7 +85,10 @@ public class TeacherPage extends AppCompatActivity {
                 }
                 if(opt.getText().toString().equals("View Attendance"))
                 {
-                    //view attendance
+                   /*
+                    startActivity(TeacherPage.this,ViewAttendance.class);
+                    finish();
+                    */
                 }
 
             }
--- app/src/main/java/com/example/attendancemanager/Teachermain2.java
+++ app/src/main/java/com/example/attendancemanager/Teachermain2.java
@@ -12,38 +12,11 @@ import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Teachermain2 extends AppCompatActivity {
-    private Spinner dpt;
-    Button nxt;
-    EditText paper, year,sec;
-    String dept;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teachermain2);
-        dpt=findViewById(R.id.department);
-        nxt=findViewById(R.id.next);
-        paper=findViewById(R.id.Papercode);
-        year=findViewById(R.id.batchyear);
-        sec=findViewById(R.id.section);
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.Department, android.R.layout.simple_spinner_item);
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dpt.setAdapter(adapter1);
-        dpt.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                dept = parent.getItemAtPosition(position).toString();
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        nxt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Teachermain2.this,TakeAttendance.class));
-            }
-        });
     }
 }
